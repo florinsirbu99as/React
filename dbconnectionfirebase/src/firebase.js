ccc42e590aff6b57eb9977baa98f210a7b71fcd6
Florin Alexandru Sirbu <florinsirbu280599@gmail.com>
2023-05-03 21:46:09 +0200

    ok

--- dbconnectionfirebase/src/firebase.js
+++ dbconnectionfirebase/src/firebase.js
@@ -1,11 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBXAbhCbBkKIWYQOLLV1_kfNEeSsr3a3fo",
-  authDomain: "epwebapp-bc171.firebaseapp.com",
-  databaseURL: "https://epwebapp-bc171-default-rtdb.firebaseio.com",
-  projectId: "epwebapp-bc171",
-  storageBucket: "epwebapp-bc171.appspot.com",
-  messagingSenderId: "640288216101",
-  appId: "1:640288216101:web:31cf48195f230dd76bdbcc",
-};
+const firebaseConfig = {};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
